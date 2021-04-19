628f5c8d160c6eb61da9c21376a83ef49899a457
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-04-20 07:13:37 +0900

    New Registration

---
+++ MVVMSample002/MVVMSample002/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MVVMSample002.ViewModels;
+using System.Windows;
+
+namespace MVVMSample002.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainWindowViewModel();
+        }
+    }
+}
