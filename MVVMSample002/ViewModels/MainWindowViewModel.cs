7f2359574db610748fa7fee8bf19a5de50d39163
emu2021makuake <81235941+emu2021makuake@users.noreply.github.com>
2021-05-18 18:48:13 +0900

    Add files via upload

---
+++ MVVMSample002/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVMSample002.ViewModels
+{
+    class MainWindowViewModel : ObservableObject
+    {
+        private bool _isFree;
+        public bool IsFree
+        {
+            get => _isFree;
+            set => SetProperty(ref _isFree, value);
+        }
+
+        private string _status;
+        public string Status
+        {
+            get => _status;
+            set => SetProperty(ref _status, value);
+        }
+
+        public ICommand ExecCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            IsFree = true;
+            Status = "";
+            ExecCommand = new AsyncRelayCommand(ExecAsync);
+        }
+
+        private async Task ExecAsync()
+        {
+            IsFree = false;
+            Status = "Processing...";
+
+            await Task.Delay(2000);
+
+            IsFree = true;
+            Status = "Complete";
+        }
+    }
+}
